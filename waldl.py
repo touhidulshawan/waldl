ef27990016c1f155f65c13b2362e78651d1e1f21
Touhidul Shawan <touhidulshawan@gmail.com>
2022-08-23 00:30:40 +0600

    handled out of page range

--- waldl.py
+++ waldl.py
@@ -6,6 +6,7 @@ from random import choices
 from string import ascii_lowercase, digits
 from collpy import cprint
 import os
+import sys
 
 # change this location according to your preference
 image_save_path = "/home/shawan/Pictures/wallhaven/" 
@@ -18,13 +19,19 @@ def search_wallpaper(query, page_number):
     res = requests.get(url)
     json_data = res.json()
 
+    last_page_number = json_data["meta"].get("last_page")
     download_path = []
 
     if len(json_data["data"]) > 0:
         for wallpaper in json_data["data"]:
             download_path.append(wallpaper["path"])
 
-    return download_path
+    data = {
+        "wallpaper_urls": download_path,
+        "last_page": last_page_number
+    }
+
+    return data 
 
 
 # generate random name
@@ -51,19 +58,27 @@ try:
     first_number, second_number = page_range.split("-")
 
     for page_number in range(int(first_number), int(second_number) + 1):
-        wallpaper_urls = search_wallpaper(query, page_number)
+        data = search_wallpaper(query, page_number)
+        wallpaper_urls = data["wallpaper_urls"]
+        last_page_number = data["last_page"]
 
-        if len(wallpaper_urls) > 0:
+
+        if len(wallpaper_urls) > 0 and page_number <= last_page_number:
             cprint(txt=f"[+] Downloading wallpapers of page: {page_number}", color="purple")
 
             for url in wallpaper_urls:
                 cprint(txt=f"[+] Downloading wallpaper: [{url}]", color="blue")
                 t = threading.Thread(target=download_wallpaper, args=(url,))
                 t.start()
+        elif page_number > last_page_number:
+            cprint(txt=f"Total Page found : {last_page_number}",
+                   color="orange")
+            break
         else:
             cprint(txt=f"404:: No image found for : {query}", color="red")
             break
         cprint(txt=f"Images saved on {image_save_path}", color="green")
 
-except Exception:
-    cprint(txt="Something not right :( | Try again.", color="red")
+except Exception as e:
+    print(sys.exc_info())
+    # cprint(txt="Something not right :( | Try again.", color="red")
