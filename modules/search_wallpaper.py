473107b035f7eb1c6d6bcc1dbca2f790a2d7fee1
Touhidul Shawan <touhidulshawan@gmail.com>
2026-04-29 18:48:44 +0600

    now wallpaper can be searched by tag id

--- modules/search_wallpaper.py
+++ modules/search_wallpaper.py
@@ -2,12 +2,17 @@ from modules.wallpaper_data import wallpaper_data
 
 
 def search_wallpaper(query: dict, page_number=1):
+    search_by = query.get("search_by")
     search_keyword = query.get("q")
     width = query.get("width")
     height = query.get("height")
     sort = query.get("sort")
     apikey = query.get("api_key")
 
+    # search by tag id
+    if search_by == "t":
+        url = f"https://wallhaven.cc/api/v1/search?q=id:{search_keyword}&page={page_number}"
+        return wallpaper_data(url)
     if query.get("purity_choice") == 1:
         url = f"https://wallhaven.cc/api/v1/search?q={search_keyword}&resolutions={width}x{height}&purity=100&sorting={sort}&order=desc&page={page_number}"
         return wallpaper_data(url)
--- modules/user_choice.py
+++ modules/user_choice.py
@@ -1,3 +1,4 @@
+import sys
 from collpy import cprint
 
 print(
@@ -7,39 +8,52 @@ print(
 =================================="""
 )
 
-example_keyword = ["digital art", "anime",
-                   "nature", "landscape", "4k", "artwork"]
-sorting_options = ["relevance", "hot",
-                   "toplist", "views", "random", "date_added"]
-cprint(txt=f"Some tags {example_keyword}", color="blue")
-tag_choice = input("[?] Enter wallpaper search keyword: ")
-cprint(txt=f"Sorting options: {sorting_options}", color="green")
-sorting_choice = input(
-    "[?] Sort image by (default -> relevance): ") or "relevance"
-print(
+cprint(txt="--> Search by Tag ID: [press t] -->", color="green")
+cprint(txt="--> Search by Keyword: [press k] --> ", color="blue")
+
+search_by = input("=> : ").lower()
+
+if search_by == "t":
+    # search by tag id 
+    tag_id = input("Enter the tag id: ")
+elif search_by == "k":
+    example_keyword = ["digital art", "anime",
+                       "nature", "landscape", "4k", "artwork"]
+    sorting_options = ["relevance", "hot",
+                       "toplist", "views", "random", "date_added"]
+    cprint(txt=f"Some tags {example_keyword}", color="blue")
+    tag_choice = input("[?] Enter wallpaper search keyword: ")
+    cprint(txt=f"Sorting options: {sorting_options}", color="green")
+    sorting_choice = input(
+        "[?] Sort image by (default -> relevance): ") or "relevance"
+    print(
+        """
+    -------------------------------------
+    | Image purity options - Default SFW |
+    -------------------------------------
+    =====================================
+    1. SFW [Safe For Work]
+    2. Sketchy
+    3. NSFW [Not Safe For Work]
+    4. All of them
+    =====================================
     """
--------------------------------------
-| Image purity options - Default SFW |
--------------------------------------
-=====================================
-1. SFW [Safe For Work]
-2. Sketchy
-3. NSFW [Not Safe For Work]
-4. All of them
-=====================================
-"""
-)
-purity_choice = int(input("Enter you choice: ") or 1)
-
-resolution_width = 1920
-resolution_height = 1080
-print("\nDefault resolution: 1920x1080")
-resolution_choice = input("Custom resolution: (y/n): ") or "n"
-
-if resolution_choice.lower() == "y":
-    resolution_width = int(input("Width: "))
-    resolution_height = int(input("Height: "))
-elif resolution_choice.lower() == "n":
-    print("Selecting default(1920x1080) resolution...")
+    )
+    purity_choice = int(input("Enter you choice: ") or 1)
+    
+    resolution_width = 1920
+    resolution_height = 1080
+    print("\nDefault resolution: 1920x1080")
+    resolution_choice = input("Custom resolution: (y/n): ") or "n"
+    
+    if resolution_choice.lower() == "y":
+        resolution_width = int(input("Width: "))
+        resolution_height = int(input("Height: "))
+    elif resolution_choice.lower() == "n":
+        print("Selecting default(1920x1080) resolution...")
+    else:
+        print("Selecting default(1920x1080) resolution...")
 else:
-    print("Selecting default(1920x1080) resolution...")
+    cprint(txt="Invalid selection", color="red")
+    sys.exit()
+
